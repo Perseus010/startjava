5f9ce23d38214712c6764e3dc35c5183a99e2438
Баранов Алексей Николаевич <Aleksey.Baranov@uf.rt.ru>
2019-11-05 12:52:49 +0300

    release Variable

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+
+	public static void main(String[] args) {
+
+		// TODO создать переменные в классе и вывести их значение на экран
+
+		byte numCores = 2;
+		short numLogProc = 4;
+		int numMemoryMb = 8;
+		long numMemoryKb = 8192; 
+		float frequency = 3.91f;
+		double averSpeedLan = 10.154584687455;
+		char versProc = 'U';
+		boolean build = true;
+
+		System.out.println(numCores);
+		System.out.println(numLogProc);
+		System.out.println(numMemoryMb);
+		System.out.println(numMemoryKb);
+		System.out.println(frequency);
+		System.out.println(averSpeedLan);
+		System.out.println(versProc);
+		System.out.println(build);
+
+	}
+}
