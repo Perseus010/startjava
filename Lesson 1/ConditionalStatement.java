8d65f12ce2f58f671cbbd0e57eeb81963d567151
Баранов Алексей Николаевич <Aleksey.Baranov@uf.rt.ru>
2019-11-27 17:10:03 +0300

    откорректирован по замечаниям

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -2,30 +2,28 @@
 public class ConditionalStatement {
 
 	public static void main(String[] args) {
-
 		int myAge = 40;
-		var gender = 'M'; // 'm' - male, 'f' - female
-		double height = 1.85;
-		var firstLetterName = 'A';
-
 		if (myAge > 20) {
 			System.out.println("Мой возраст свыше 20 лет");
 		}
 
-		if (gender == 'M') {
+		char gender = 'M'; //M - мужской пол
+		boolean isCheckGender = gender == 'M' ;
+		if (isCheckGender == true) {
 			System.out.println("Перед Вами лицо мужского пола");
-		}
+		} 
+		if (isCheckGender == false) {
+			System.out.println("Перед Вами лицо не мужского пола");
+		} 
 
-		if (gender != 'M') {
-			System.out.println("Перед Вами лицо женского пола");
-		}
-
+		double height = 1.85;
 		if (height < 1.80) {
 			System.out.println("Ваш рост меньше 180 см");
 		} else {
 			System.out.println("Ваш рост свыше 180 см");
 		}
 
+		char firstLetterName = 'A';
 		if (firstLetterName == 'M') {
 			System.out.println("Ваше имя начинается на букву M");
 		} else if(firstLetterName == 'I') {
@@ -33,7 +31,5 @@ public class ConditionalStatement {
 		} else {
 			System.out.println("Ваше имя начинается не на букву M или I");
 		}
-
 	}
-
 }
