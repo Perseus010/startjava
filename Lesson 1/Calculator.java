e9d6a3a8923f41adaeafcfdd1ea88705cfbc366d
Баранов Алексей Николаевич <Aleksey.Baranov@uf.rt.ru>
2019-12-11 11:26:11 +0300

    исправление по калькулятору

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -9,31 +9,25 @@ public class Calculator {
 		if (operat == '+') {
 			result = num1 + num2;
 			System.out.println("Сумма значений " + num1 + " и " + num2 + " равна " + result);
-		} 
-		else if (operat == '-') {
+		} else if (operat == '-') {
 			result = num1 - num2;
 			System.out.println("Разность значений " + num1 + " и " + num2 + " равна " + result);
-		}
-		else if (operat == '*') {
+		} else if (operat == '*') {
 			result = num1 * num2;
 			System.out.println("Произведение значений " + num1 + " и " + num2 + " равно " + result);
-		}
-		else if (operat == '/') {
+		} else if (operat == '/') {
 			result = num1 / num2;
 			System.out.println("Частное значения " + num1 + " от " + num2 + " составляет " + result);
-		}
-		else if (operat == '^') {
+		} else if (operat == '^') {
 			result = 1;
 			for (int i = 1; i <= num2; i++) {
-				result = result * num1;
+				result *= num1;
 			}
 			System.out.println("Возведение значения " + num1 + " в степень " + num2 + " равно " + result);			
-		}
-		else if (operat == '%') {
+		} else if (operat == '%') {
 			result = num1 % num2;
 			System.out.println("Остаток от деления значений " + num1 + " и " + num2 + " равен " + result);
-		} 
-		else {
+		} else {
 			System.out.println("Допущена ошибка при вводе опеератора. Попробуйте внести значение вновь");
 		}
 	}
